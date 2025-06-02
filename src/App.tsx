4bc61d75eed99d836c1256142fa78d83fcf25bab
efar716 <110000165+efar716@users.noreply.github.com>
2025-06-03 01:18:04 +1200

    added background + font + first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import LandingPage from './pages/LandingPage/LandingPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>)
 }
 
 export default App
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,21 @@
+const LandingPage = () => {
+
+
+    return (
+
+
+
+        <main>
+
+            <h1>
+                INDIFY
+            </h1>
+
+
+        </main>
+
+    );
+
+}
+
+export default LandingPage;
