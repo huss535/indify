7c062f589a9e6bd1bab84a14571070b25db675ff
efar716 <110000165+efar716@users.noreply.github.com>
2025-06-14 14:28:07 +1200

    Created Album Cover Component

--- src/pages/AlbumRevealPage/AlbumRevealPage.tsx
+++ src/pages/AlbumRevealPage/AlbumRevealPage.tsx
@@ -1,9 +1,18 @@
 import './AlbumRevealPage.css'
 const AlbumRevealPage = () => {
     return (
-        <div>
-            <h1>Album Reveal</h1>
-        </div>
+        <main>
+            <h2>Album Reveal</h2>
+            <div id='album-container' className='shadow-effect '>
+                {/*   <div className='album-cover'>
+                    <img src='https://via.placeholder.com/300' alt='Album Cover' />
+                </div> */}
+                <img id='album-cover' src='public/images/album-photos/album-cover.png' alt='Album Cover' />
+                <img id='parental-advisory-logo' src='public/images/album-photos/parental-advisory-icon.svg' alt='Parental Advisory Logo' />
+                <h2>Chloe's indified starter pack</h2>
+
+            </div>
+        </main>
     );
 };
 
