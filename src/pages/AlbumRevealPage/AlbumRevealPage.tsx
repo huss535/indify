4a44b5bf4c2588a9c0921035aea13a72478139d8
efar716 <110000165+efar716@users.noreply.github.com>
2025-06-14 19:43:22 +1200

    Created header area + Album Cover

--- src/pages/AlbumRevealPage/AlbumRevealPage.tsx
+++ src/pages/AlbumRevealPage/AlbumRevealPage.tsx
@@ -7,6 +7,7 @@ const AlbumRevealPage = () => {
 
             <h1 className='user-info-header'>
                 <span>Indify</span>
+                <img src='public/images/x-icon.png' alt='X Icon' />
                 Chloe
                 <span>Present</span>
 
@@ -14,9 +15,7 @@ const AlbumRevealPage = () => {
 
 
             <div id='album-container' className='shadow-effect '>
-                {/*   <div className='album-cover'>
-                    <img src='https://via.placeholder.com/300' alt='Album Cover' />
-                </div> */}
+
                 <img id='album-cover' src='public/images/album-photos/album-cover.png' alt='Album Cover' />
                 <img id='parental-advisory-logo' src='public/images/album-photos/parental-advisory-icon.svg' alt='Parental Advisory Logo' />
                 <h2>Chloe's indified starter pack</h2>
